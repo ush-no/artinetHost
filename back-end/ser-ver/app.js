65214de2830a285149418f782746bb0b07467a2f
Ushno <149103418+ush-no@users.noreply.github.com>
2024-05-14 15:14:32 +0600

    Update app.js

--- back-end/ser-ver/app.js
+++ back-end/ser-ver/app.js
@@ -10,7 +10,7 @@ const app = express();
     methods: ['GET','POST', 'PUT', 'DELETE'], 
    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, }) ];
-app. use(options);` */. 
+app. use(options);` */
 
 
 
