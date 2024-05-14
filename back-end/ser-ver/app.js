1912e33a745187bae5f6fa39993ec02341916f70
Ushno <149103418+ush-no@users.noreply.github.com>
2024-05-14 13:11:26 +0600

    Update app.js

--- back-end/ser-ver/app.js
+++ back-end/ser-ver/app.js
@@ -5,7 +5,16 @@ import bodyParser from "body-parser";
 
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' })); 
+
+const options = [ cors({ origin:'http://localhost:5173', 
+    methods: ['GET','POST', 'PUT', 'DELETE'], 
+   allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, }) ];
+app. use(options);`.
+
+
+
+/* app.use(cors({ origin: 'http://localhost:5173' }));  */
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
