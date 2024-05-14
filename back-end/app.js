4ba546eace1cc6c9854b269be94fda79a636db15
Ushno <149103418+ush-no@users.noreply.github.com>
2024-05-14 17:31:53 +0600

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -68,8 +68,8 @@ app.post("/api/posts", async(req, res) => {
 
 
 
-const port = process.env.PORT || 3000; // Default to 3000 if PORT is not set
+/* const port = process.env.PORT || 3000; // Default to 3000 if PORT is not set
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+}); */
